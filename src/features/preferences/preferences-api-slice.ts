65a776406213320aeb66569c77de6ed4a5e331bc
saadfrhan <saadfarhan347@gmail.com>
2024-04-24 11:44:42 +0500

    Update preferences-api-slice.ts

--- src/features/preferences/preferences-api-slice.ts
+++ src/features/preferences/preferences-api-slice.ts
@@ -7,7 +7,7 @@ export const preferencesApiSlice = createApi({
     baseUrl:
       process.env.NODE_ENV === "production"
         ? "https://tmdb-backend-ruby.vercel.app"
-        : "http://localhost:4000",
+        : "http://localhost:3000",
   }),
   endpoints(builder) {
     return {

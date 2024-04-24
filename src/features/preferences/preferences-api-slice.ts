2de61c378b688863ca8a2d76949c17b3bc8203e3
saadfrhan <saadfarhan347@gmail.com>
2024-04-24 12:06:28 +0500

    Update preferences-api-slice.ts

--- src/features/preferences/preferences-api-slice.ts
+++ src/features/preferences/preferences-api-slice.ts
@@ -5,7 +5,7 @@ export const preferencesApiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl:
       process.env.NODE_ENV === "production"
-        ? "http://tmdb-backend-ruby.vercel.app"
+        ? "https://tmdb-backend-ruby.vercel.app"
         : "http://localhost:3000",
     prepareHeaders(headers) {
       headers.set("Access-Control-Allow-Origin", "*");
